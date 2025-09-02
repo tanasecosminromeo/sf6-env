c97d10a85f0aabd73b24e4ea645b3acfa989bf35
Cosmin-Romeo TANASE <cosmin@tanase.dev>
2025-09-02 10:20:33 +0300

    .bash - Change alias to use the app user for CLI commands

--- .bash
+++ .bash
@@ -1,4 +1,4 @@
 alias a="docker compose exec app $@"
 alias d="docker compose exec db $@"
-alias p="docker compose exec app php bin/console $@"
-alias c="docker compose exec app composer $@"
+alias p="docker compose exec --user app app php bin/console $@"
+alias c="docker compose exec --user app app composer $@"
